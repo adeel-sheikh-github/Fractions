a1f1eb8d217c37001e4fc98818a989e10cb76c40
adeel-sheikh-github <85794334+adeel-sheikh-github@users.noreply.github.com>
2021-06-12 08:42:01 -0500

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,203 @@
+﻿using System.Text;
+
+namespace FractionTest
+{
+    class Fraction
+    {
+        //a is numerator
+        //b is denominator
+        private int a, b;
+
+        #region Constructors
+
+        public Fraction ()
+        {
+            a = 0;
+            b = 1;
+        }
+
+        /// <summary>
+        /// Create a new Fraction
+        /// </summary>
+        /// <param name="a">Numerator</param>
+        /// <param name="b">Denominator</param>
+        public Fraction(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        /// <summary>
+        /// Create a new fraction from a whole number
+        /// </summary>
+        /// <param name="a">Numerator. Denominator will be 1</param>
+        public Fraction(int a)
+        {
+            this.a = a;
+            this.b = 1;
+        }
+
+        /// <summary>
+        /// Create a copy of fraction x
+        /// </summary>
+        /// <param name="x"></param>
+        public Fraction(Fraction x)
+        {
+            this.a = x.a;
+            this.b = x.b;
+        }
+#endregion
+
+        #region Arithmatic Operations
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Fraction Multiply(Fraction other)
+        {
+            Fraction result = new Fraction();
+            result.a = this.a * other.a;
+            result.b = this.b * other.b;
+            return result;
+        }
+
+        public Fraction DividedBy(Fraction other)
+        {
+            Fraction result = new Fraction();
+            result.a = this.a * other.b;
+            result.b = this.b * other.a;
+            return result;
+        }
+
+        public Fraction Add(Fraction other)
+        {
+            Fraction result = new Fraction();
+            result.a = this.a * other.b + other.a * this.b;
+            result.b = this.b * other.b;
+            return result;
+        }
+
+        public Fraction Subtract(Fraction other)
+        {
+            Fraction result = new Fraction();
+            result.a = this.a * other.b - other.a * this.b;
+            result.b = this.b * other.b;
+            return result;
+        }
+
+
+        #endregion
+
+        #region Utility Functions
+
+        public bool Equal(Fraction other)
+        {
+            return this.a == other.a && this.b == other.b;
+        }
+        public bool NotEqual(Fraction other)
+        {
+            return !Equal(other);
+        }
+
+        public bool GreaterThan(Fraction other)
+        {
+            double x = (double)this.a / this.b;
+            double y = (double)other.b / other.b;
+            return x > y;
+        }
+
+        public bool GreaterThanOrEqual(Fraction other)
+        {
+            return Equal(other) || GreaterThan(other);
+        }
+
+        public bool LessThan(Fraction other)
+        {
+            return !GreaterThanOrEqual(other);
+        }
+
+        public bool LessThanOrEqual(Fraction other)
+        {
+            return !GreaterThan(other);
+        }
+
+        /// <summary>
+        /// Greatest Common Divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Gcd(int a, int b)
+        {
+            int t = 0;
+            while (b != 0)
+            {
+                t = b;
+                b = a % b;
+                a = t;
+            }
+
+            return a;
+        }
+
+        public void Simplify()
+        {
+            int divisor = Gcd(this.a, this.b);
+            this.a = this.a / divisor;
+            this.b = this.b / divisor;
+        }
+
+        public Fraction Reduced()
+        {
+            Fraction result = new Fraction();
+            int divisor = Gcd(this.a, this.b);
+            result.a = this.a / divisor;
+            result.b = this.b / divisor;
+            return result;
+        }
+
+  
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            var localNumerator = a;
+
+            // If the numerator is more than the denominator then there will be a whole part
+
+            if (System.Math.Abs(localNumerator) >= b)
+            {
+                if (b == 0)
+                {
+                   stringBuilder.Append("Divide by Zero not allowed");
+                   return stringBuilder.ToString();
+                }
+                int wholePart = localNumerator / b;
+                localNumerator -= (wholePart * b);
+                stringBuilder.Append(wholePart);
+
+                if (localNumerator != 0)
+                {
+                    stringBuilder.Append('_');
+                    localNumerator = System.Math.Abs(localNumerator);
+                }
+            }
+
+            // If there is anything left in the numerator, append the fractional part
+
+            if (localNumerator != 0)
+            {
+                stringBuilder.Append(localNumerator);
+                stringBuilder.Append('/');
+                stringBuilder.Append(b);
+            }
+
+            return stringBuilder.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FractionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAllUnitTests();
+            Console.ReadLine();
+        }
+
+        #region Test
+
+        public static void RunAllUnitTests()
+        {
+            var inputStrings = new[]
+            {
+                "1/2 * 3_3/4",
+                "2_3/8 + 9/8",
+                "2_3/8   +     9/8",
+                "7/2 * 111",
+                "10/6 + 21/23",
+                "1/3 + 1/5",
+                "4/5 - 1/5",
+                "1/4 / 1/2",
+                "-1/4 + 5/4",
+                "-1/3 - 1/3",
+                "1/0 - 2/4",
+                "1/3 / 0/4",
+            };
+
+            foreach (var input in inputStrings)
+            {
+                RunUnitTest(input);
+            }
+        }
+
+        private static void RunUnitTest(string input)
+        {
+            List<string> listofInput = input.Split(' ').ToList();
+            listofInput = listofInput.Where(item => !string.IsNullOrWhiteSpace(item)).Distinct().ToList();
+
+            Fraction x = ConvertMixedToImproperFraction(listofInput[0]);
+            var _operator = listofInput[1];
+            Fraction y = ConvertMixedToImproperFraction(listofInput[2]);
+
+            switch (_operator)
+            {
+                case "+":
+                    Console.WriteLine("Result of {0} = {1}", input, x.Add(y).Reduced());
+                    break;
+                case "-":
+                    Console.WriteLine("Result of {0} = {1}", input, x.Subtract(y).Reduced());
+                    break;
+                case "/":
+                    if (y.Equal(new Fraction(0)))
+                        Console.WriteLine("Divide by Zero 0 is not allowed!!!");
+                    else
+                        Console.WriteLine("Result of {0} = {1}", input, x.DividedBy(y).Reduced());
+                    break;
+                case "*":
+                    Console.WriteLine("Result of {0} = {1}", input, x.Multiply(y).Reduced());
+                    break;
+                case "x":
+                    Console.WriteLine("Result of {0} = {1}", input, x.Multiply(y).Reduced());
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Utility
+        private static Fraction ConvertMixedToImproperFraction(string s)
+        {
+            if (!s.Contains("_"))
+            {
+                return StringToFraction(s);
+            }
+
+            int wholeNumber = Convert.ToInt32(s.Substring(0, s.IndexOf("_")));
+            int numerator = Convert.ToInt32(s.Substring(s.IndexOf("_") + 1, s.IndexOf("/") - s.IndexOf("_") - 1));
+            int denominator = Convert.ToInt32(s.Substring(s.IndexOf("/") + 1));
+
+            return new Fraction(denominator * wholeNumber + numerator, denominator);
+        }
+
+        static Fraction StringToFraction(string str)
+        {
+            string[] split = str.Split(new char[] { ' ', '/' });
+
+            if (split.Length == 1)
+                return new Fraction(Convert.ToInt32(split[0]));
+
+            if (split.Length == 2)
+            {
+                int a, b;
+
+                if (int.TryParse(split[0], out a) && int.TryParse(split[1], out b))
+                {
+                    if (split.Length == 2)
+                    {
+                        return new Fraction(a, b);
+                    }
+                }
+            }
+
+            throw new FormatException("Not a valid fraction.");
+        }
+
+
+        #endregion
+
+    }
+}
